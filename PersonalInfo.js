5c0f4a10f60e753d20757e169e2fcfea6c384c88
Matthew A Rosales <matthewrosales@usf.edu>
2024-07-05 18:44:38 -0400

    Add personal information section with styled components

---
+++ PersonalInfo.js
@@ -0,0 +1,14 @@
+// U22978120
+
+import React from 'react';
+import styled from 'styled-components';
+
+function PersonalInfo() {
+  return (
+    <Wrapper>
+      <Name>Matthew Rosales</Name>
+      <Bio> I am a business analytics major at the university of south florida. I also work full time for a fortune 100 company called tdsynnex. I work as a new vendor onboarder and love what i do.</Bio>
+      <Link href="https://www.linkedin.com/in/matthew-rosales-3b86a62a5/">LinkedIn</Link>
+    </Wrapper>
+  );
+}
