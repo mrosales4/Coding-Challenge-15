2f7e6aa19b424cd4328a321d8e98e6727b862bd7
Matthew A Rosales <matthewrosales@usf.edu>
2024-07-05 18:47:47 -0400

    Create experience section with reusable components

---
+++ Experience.js
@@ -0,0 +1,12 @@
+//U22978120
+
+import React from 'react';
+import styled from 'styled-components';
+
+const experiences = [
+  {
+    title: 'TDsynnex',
+    company: 'Amazing Company to work for',
+    duration: '2024 - Present',
+  },
+];
