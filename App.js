ef5e1821169a9b2fe77ac1ea9e67a836ee8296c8
Matthew A Rosales <matthewrosales@usf.edu>
2024-07-05 18:41:59 -0400

    Create App.js to import all info

---
+++ App.js
@@ -0,0 +1,19 @@
+// U22978120
+
+import React from 'react';
+import PersonalInfo from './PersonalInfo';
+import Skills from './Skills';
+import Experience from './Experience';
+import Education from './Education';
+
+function App() {
+  return (
+    <div className="App">
+      <PersonalInfo />
+      <Skills />
+      <Experience />
+      <Education />
+    </div>
+  );
+}
+
