cc99388097d49689fe505c1c985b623de9803223
Matthew A Rosales <matthewrosales@usf.edu>
2024-07-05 18:51:53 -0400

    Display educational background in a dedicated section.

---
+++ Education.js
@@ -0,0 +1,14 @@
+// U22978120
+
+import React from 'react';
+import styled from 'styled-components';
+
+const Education = () => {
+  const education = [
+    {
+      institution: 'University Of South Florida',
+      degree: 'Bachelor of Science in Business Analytics and Information Systems',
+      years: '2022 - 2025',
+    },
+    ]
+
